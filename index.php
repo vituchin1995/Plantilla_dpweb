df0ef633e35f9473bdbac1fa641d93576305e938
vituchin1995 <115170951+vituchin1995@users.noreply.github.com>
2023-04-19 09:44:08 -0500

    Subir primera plantilla

---
+++ index.php
@@ -0,0 +1,118 @@
+<?php include'includes/heider.php'?>
+
+<!---
+<div class="col-lg-4 col-sm-12 col-md-6 md-3">
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="psg2023.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  
+</div>
+</div>
+--->
+<br>
+<br>
+<br>
+
+<br>
+<br>
+
+<div class="container my=3">
+
+<center>
+    <h1>PSG FC</h1> <br>
+</center>
+    
+     <div class="row">
+
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="psg1.jpg" alt="Imagen 1" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Primer Equipo</h5>
+             <p class="card-text">El Paris Saint-Germain Football Club, conocido popularmente por sus siglas PSG, es una entidad polideportiva francesa con sede en París.</p>
+             <a href="#" class="btn btn-primary">Ver Ahora</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="psg2.jpg" alt="Imagen 2" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Primer Equipo</h5>
+             <p class="card-text">El Paris Saint-Germain Football Club, conocido popularmente por sus siglas PSG, es una entidad polideportiva francesa con sede en París.</p>
+             <a href="#" class="btn btn-primary">Ver Ahora</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+      
+      
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="psg3.jpg" alt="Imagen 3" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Primer Equipo</h5>
+             <p class="card-text">El Paris Saint-Germain Football Club, conocido popularmente por sus siglas PSG, es una entidad polideportiva francesa con sede en París.</p>
+             <a href="#" class="btn btn-primary">Ver Ahora</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+        </div>
+        
+       
+     
+     
+    
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="psg4.jpg" alt="Imagen 4" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Primer Equipo</h5>
+             <p class="card-text">El Paris Saint-Germain Football Club, conocido popularmente por sus siglas PSG, es una entidad polideportiva francesa con sede en París.</p>
+             <a href="#" class="btn btn-primary">Ver Ahora</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+      
+       
+      
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="psg5.jpg" alt="Imagen 5" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Primer Equipo</h5>
+             <p class="card-text">El Paris Saint-Germain Football Club, conocido popularmente por sus siglas PSG, es una entidad polideportiva francesa con sede en París.</p>
+             <a href="#" class="btn btn-primary">Ver Ahora</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+       
+       <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+         <div class="card">
+           <img src="psg1.jpg" alt="Imagen 6" class="card-img-top">
+           <div class="card-body">
+             <h5 class="card-title">Primer Equipo</h5>
+             <p class="card-text">El Paris Saint-Germain Football Club, conocido popularmente por sus siglas PSG, es una entidad polideportiva francesa con sede en París.</p>
+             <a href="#" class="btn btn-primary">Ver Ahora</a>
+             <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+           </div>
+         </div>
+       </div>
+       </div> 
+       </div><br>
+      
+<?php include'includes/footer.php'?>
---
+++ index2.php
@@ -0,0 +1,120 @@
+<?php include'includes/heider.php'?>
+<!---
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./Galerias2/barcelona.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  
+</div>
+<br>
+--->
+      <!---cards-->
+     
+
+
+
+
+      <br>
+<br>
+<br>
+
+<br>
+<br>
+
+      <div class="container my=3">
+    <center>
+    <h1>FC BARCELONA</h1> <br>
+    </center>
+   
+    <div class="row">
+
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+        <div class="card">
+          <img src="./Galerias2/b1.jpg" alt="Imagen 1" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Primer Equipo</h5>
+            <p class="card-text">El Fútbol Club Barcelona, conocido popularmente como Barça, ​ es una entidad polideportiva con sede en Barcelona, Cataluña, España.</p>
+            <a href="#" class="btn btn-primary">Ver Ahora</a>
+            <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+          </div>
+        </div>
+      </div>
+     
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+        <div class="card">
+          <img src="./Galerias2/b2.jpg" alt="Imagen 2" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Primer Equipo</h5>
+            <p class="card-text">El Fútbol Club Barcelona, conocido popularmente como Barça, ​ es una entidad polideportiva con sede en Barcelona, Cataluña, España.</p>
+            <a href="#" class="btn btn-primary">Ver Ahora</a>
+            <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+          </div>
+        </div>
+      </div>
+     
+     
+     
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+        <div class="card">
+          <img src="./Galerias2/b3.jpg" alt="Imagen 3" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Primer Equipo</h5>
+            <p class="card-text">El Fútbol Club Barcelona, conocido popularmente como Barça, ​ es una entidad polideportiva con sede en Barcelona, Cataluña, España.</p>
+            <a href="#" class="btn btn-primary">Ver Ahora</a>
+            <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+          </div>
+        </div>
+       </div>
+       
+      
+    
+    
+   
+     <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+        <div class="card">
+          <img src="./Galerias2/b4.jpg" alt="Imagen 4" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Primer Equipo</h5>
+            <p class="card-text">El Fútbol Club Barcelona, conocido popularmente como Barça, ​ es una entidad polideportiva con sede en Barcelona, Cataluña, España.</p>
+            <a href="#" class="btn btn-primary">Ver Ahora</a>
+            <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+          </div>
+        </div>
+      </div>
+     
+      
+     
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+        <div class="card">
+          <img src="./Galerias2/b5.jpg" alt="Imagen 5" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Primer Equipo</h5>
+            <p class="card-text">El Fútbol Club Barcelona, conocido popularmente como Barça, ​ es una entidad polideportiva con sede en Barcelona, Cataluña, España.</p>
+            <a href="#" class="btn btn-primary">Ver Ahora</a>
+            <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+          </div>
+        </div>
+      </div>
+      
+      <div class="col-lg-4 col-sm-12 col-md-6 md-3">
+        <div class="card">
+          <img src="./Galerias2/b1.jpg" alt="Imagen 6" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title">Primer Equipo</h5>
+            <p class="card-text">El Fútbol Club Barcelona, conocido popularmente como Barça, ​ es una entidad polideportiva con sede en Barcelona, Cataluña, España.</p>
+            <a href="#" class="btn btn-primary">Ver Ahora</a>
+            <!---<a href="#" class="btn btn-primary">Añadir al carrito</a>--->
+          </div>
+        </div>
+      </div>
+      </div> 
+      </div><br>
+<?php include'includes/footer.php'?>
